686377f596a7f527ce8da127c070c15a2547cdba
Mohammad Salah Akram Fuad <120177637+salahakramfuad@users.noreply.github.com>
2025-03-01 11:57:08 +0600

    Added odd even check

---
+++ sayor.py
@@ -0,0 +1,11 @@
+def check_even_odd(number):
+  if number % 2 == 0:
+    return "Even"
+  else:
+    return "Odd"
+
+# Example usage
+if __name__ == "__main__":
+  num = int(input("Enter a number: "))
+  result = check_even_odd(num)
+  print(f"The number {num} is {result}.")
