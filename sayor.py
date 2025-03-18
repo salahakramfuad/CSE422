4ed9d1cee01c0d831004ae005b0b2c53b4116bc1
Mohammad Salah Akram Fuad <120177637+salahakramfuad@users.noreply.github.com>
2025-03-18 20:58:55 +0600

    shortest path

--- sayor.py
+++ sayor.py
@@ -1,3 +1,5 @@
+import heapq
+
 class Node:
   def __init__(self, data):
     self.data = data
@@ -56,3 +58,49 @@ dll.prepend(5)
 dll.display()
 dll.delete(10)
 dll.display()
+
+
+
+
+
+
+class Graph:
+  def __init__(self):
+    self.edges = {}
+
+  def add_edge(self, from_node, to_node, weight):
+    if from_node not in self.edges:
+      self.edges[from_node] = []
+    self.edges[from_node].append((to_node, weight))
+
+  def dijkstra(self, start):
+    distances = {node: float('inf') for node in self.edges}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+
+    while priority_queue:
+      current_distance, current_node = heapq.heappop(priority_queue)
+
+      if current_distance > distances[current_node]:
+        continue
+
+      for neighbor, weight in self.edges[current_node]:
+        distance = current_distance + weight
+
+        if distance < distances[neighbor]:
+          distances[neighbor] = distance
+          heapq.heappush(priority_queue, (distance, neighbor))
+
+    return distances
+
+
+# Example usage
+graph = Graph()
+graph.add_edge('A', 'B', 1)
+graph.add_edge('A', 'C', 4)
+graph.add_edge('B', 'C', 2)
+graph.add_edge('B', 'D', 6)
+graph.add_edge('C', 'D', 3)
+
+distances = graph.dijkstra('A')
+print(distances)
